6996e76a63e56b204da7511efbf4b7c50caee762
thongle <lpthong90@gmail.com>
2016-04-04 10:32:29 +0700

    Update test case 20160331 - 002

--- 20160331 - 002/config.py
+++ 20160331 - 002/config.py
@@ -120,7 +120,7 @@ model_config = {
     'layers': [(100, 'Sigmoid'), (3, 'Softmax')],
     'learning_rate': 0.01,
     'learning_rule': 'adagrad',
-    'n_iter': 100
+    'n_iter': 300
 }
 model_config_file = 'model_config'
 
